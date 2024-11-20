f06b87cc78d3f75e2af5bc2aa7e3de1b06d44727
devswi <stayfocusjs@gmail.com>
2024-11-20 09:48:10 +0800

    feat: reset project

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
